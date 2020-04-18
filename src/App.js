484638e94abcd49dbcc453a8a6b4da47cbab4806
Vimanyu Awal <awal@usc.edu>
2020-04-18 15:40:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar/NavBar'
+import Home from "./Home/Home"
+import Footer from './Footer/Footer'
+import Company from './Company/Company'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
